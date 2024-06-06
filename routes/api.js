629373f94f523193a2441da9b73829c73a343fd1
Sami Fink <fink.s.m.8651@gmail.com>
2024-06-06 20:48:18 +0000

    feat: added fetch for stock price

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,32 @@
 'use strict';
 
+async function getPrice(stock) {
+  const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`;
+  try {
+    const res = await fetch(url);
+    const info = await res.json();
+    return info.latestPrice;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function (req, res){
+      let stocks = req.query.stock;
+      if (!Array.isArray(stocks)){
+        stocks = [stocks];
+      }
+
+      let prices = [];
+      for (let stock of stocks) {
+        let price = await getPrice(stock)
+        prices.push(price);
+      }
+
+      console.log(prices);
     });
     
 };
