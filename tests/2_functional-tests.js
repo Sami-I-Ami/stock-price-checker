15ab14ef0ba184be2dab28485d64cf1a04238642
Sami Fink <fink.s.m.8651@gmail.com>
2024-06-08 01:22:31 +0000

    feat: completed functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,85 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    test('Viewing one stock', function(done) {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/stock-prices?stock=GOOG')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+                assert.isObject(res.body.stockData);
+                assert.equal(res.body.stockData.stock, "GOOG");
+                assert.exists(res.body.stockData.price);
+                assert.exists(res.body.stockData.likes);
+                done();
+            })
+    });
+    test('Viewing one stock and liking it', function(done) {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/stock-prices?stock=GOOG&like=true')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+                assert.isObject(res.body.stockData);
+                assert.equal(res.body.stockData.stock, "GOOG");
+                assert.exists(res.body.stockData.price);
+                assert.equal(res.body.stockData.likes, 1);
+                done();
+            })
+    });
+    test('Viewing one stock and liking it again', function(done) {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/stock-prices?stock=GOOG&like=true')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+                assert.isObject(res.body.stockData);
+                assert.equal(res.body.stockData.stock, "GOOG");
+                assert.exists(res.body.stockData.price);
+                assert.equal(res.body.stockData.likes, 1);
+                done();
+            })
+    });
+    test('Viewing two stocks', function(done) {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/stock-prices?stock=GOOG&stock=MSFT')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+                assert.isArray(res.body.stockData);
+                assert.equal(res.body.stockData[0].stock, "GOOG");
+                assert.exists(res.body.stockData[0].price);
+                assert.equal(res.body.stockData[0].rel_likes, 1);
+                assert.equal(res.body.stockData[1].stock, "MSFT");
+                assert.exists(res.body.stockData[1].price);
+                assert.equal(res.body.stockData[1].rel_likes, -1);
+                done();
+            })
+    });
+    test('Viewing two stocks and liking them', function(done) {
+        chai
+            .request(server)
+            .keepOpen()
+            .get('/api/stock-prices?stock=GOOG&stock=MSFT&like=true')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+                assert.isArray(res.body.stockData);
+                assert.equal(res.body.stockData[0].stock, "GOOG");
+                assert.exists(res.body.stockData[0].price);
+                assert.equal(res.body.stockData[0].rel_likes, 0);
+                assert.equal(res.body.stockData[1].stock, "MSFT");
+                assert.exists(res.body.stockData[1].price);
+                assert.equal(res.body.stockData[1].rel_likes, 0);
+                done();
+            })
+    });
 });
